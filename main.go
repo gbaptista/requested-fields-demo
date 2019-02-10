f5299e939bea5d87235e6c63019ee5cf3f48cc38
gbaptista <guilhermebaptistasilva@gmail.com>
2019-02-10 18:06:46 -0200

    first commit

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	fields "github.com/gbaptista/requested-fields"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
+	"github.com/go-chi/render"
+	graphql "github.com/graph-gophers/graphql-go"
+	"net/http"
+)
+
+const schema = `
+	schema {
+		query: Query
+	}
+
+	type Query {
+		user: User
+	}
+
+	type User {
+		name: String
+		address: Address
+	}
+
+	type Address {
+		city: String
+		street: String
+	}
+`
+
+var parsedSchema = graphql.MustParseSchema(schema, &Query{})
+
+type GraphqlParams struct {
+	Query         string                 `json:"query"`
+	OperationName string                 `json:"operationName"`
+	Variables     map[string]interface{} `json:"variables"`
+}
+
+func graphqlRequestHandler(writer http.ResponseWriter, request *http.Request) {
+	var params GraphqlParams
+
+	json.NewDecoder(request.Body).Decode(&params)
+
+	ctx := context.WithValue(request.Context(),
+		"graphqlRequestTree", fields.BuildTree(params.Query))
+
+	response := parsedSchema.Exec(
+		ctx, params.Query, params.OperationName, params.Variables)
+
+	jsonResponse, _ := json.Marshal(response)
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.Write(jsonResponse)
+}
+
+func main() {
+	router := chi.NewRouter()
+
+	cors := cors.New(cors.Options{AllowedOrigins: []string{"*"}})
+
+	router.Use(
+		cors.Handler, middleware.Logger,
+		render.SetContentType(render.ContentTypeJSON),
+	)
+
+	router.Post("/graphql", graphqlRequestHandler)
+
+	http.ListenAndServe(":3000", router)
+}
---
+++ resolvers.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	fields "github.com/gbaptista/requested-fields"
+	"log"
+)
+
+type Query struct {
+	Field fields.Field `graphql:"query"`
+}
+
+type UserResolver struct {
+	Field fields.Field `graphql:"user"`
+}
+
+type AddressResolver struct {
+	Field fields.Field `graphql:"address"`
+}
+
+func (queryResolver *Query) User(ctx context.Context) *UserResolver {
+	userResolver := &UserResolver{}
+	userResolver.Field.SetParent(queryResolver)
+
+	log.Println("Query.User Fields: %v", fields.RequestedFor(ctx, userResolver))
+
+	return userResolver
+}
+
+func (userResolver *UserResolver) Name(ctx context.Context) *string {
+	name := "Harry Potter"
+
+	return &name
+}
+
+func (userResolver *UserResolver) Address(ctx context.Context) *AddressResolver {
+	addressResolver := &AddressResolver{}
+	addressResolver.Field.SetParent(userResolver)
+
+	log.Println("User.Address Fields: %v",
+		fields.RequestedFor(ctx, addressResolver))
+
+	return addressResolver
+}
+
+func (addressResolver *AddressResolver) City(ctx context.Context) *string {
+	city := "Little Whinging"
+
+	return &city
+}
+
+func (addressResolver *AddressResolver) Street(ctx context.Context) *string {
+	street := "4 Privet Drive"
+
+	return &street
+}
